efbaa82a3fed2b43a5c0897831a44e84e5af7567
balkheezbanu-ux <balkheezbanu@gmail.com>
2026-09-13 13:56:40 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE="nandini-milk-v1";
 const ASSETS=[
   "./",
-  "./asdf_bill_custom_fixed.html",
+  "./index.html",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
