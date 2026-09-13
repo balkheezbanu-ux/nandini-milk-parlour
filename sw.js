9bf0974e61e5ae7e9972cd48b3e0b7185b48cbe4
balkheezbanu-ux <balkheezbanu@gmail.com>
2026-09-13 13:39:48 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE="nandini-milk-v1";
+const ASSETS=[
+  "./",
+  "./asdf_bill_custom_fixed.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET") return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(resp=>{
+    const copy=resp.clone();
+    caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});
+    return resp;
+  }).catch(()=>cached)));
+});
